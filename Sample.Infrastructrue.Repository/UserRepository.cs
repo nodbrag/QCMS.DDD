c7c13f2b75bec612eb9e1147a0192997491a4d61
jime.zhu <nodbrag@sina.com>
2019-05-10 16:56:15 +0800

    add UserRepository

---
+++ Sample.Infrastructrue.Repository/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sample.Domain.Repositories;
+
+namespace Sample.Infrastructrue.Repository
+{
+    public class UserRepository:RepositoryBase,IUserRepository
+    {
+
+    }
+}
